1d0aee2866bdb51b5826b7b72224097d2601c479
Kristian Lukacovic <17kristian@gmail.com>
2024-03-19 00:30:42 +0100

    second

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import { useMemo, useState } from "react";
 import Chess from "chess.js";
 import { Chessboard } from "react-chessboard";
-import { buttonStyle,boardWrapper,stockfishMoveButton } from './style';
+import { buttonStyle,stockfishMoveButton,appContainer,sideContainer,chessboardContainer } from './style';
 import axios from 'axios';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importujte štýly Bootstrap
+
 
 function App() {
   const game = useMemo(() => new Chess(), []);
@@ -11,31 +15,20 @@ function App() {
   const [evaluation, setEvaluation] = useState('');
   const [opening, setOpening] = useState({});
   const [isActive, setIsActive] = useState(false);
+  // Stav pre sledovanie orientácie šachovnice
+  const [boardOrientation, setBoardOrientation] = useState('white');
+  const [depth, setDepth] = useState(13); // Default depth
+  const [showModal, setShowModal] = useState(false);
+  const [resultMessage, setResultMessage] = useState('');
+  
 
-  async function getOpening() {
-    const currentFen = game.fen();
-    const currentFenPosition = currentFen.split(' ')[0]; // Berieme len prvú časť FEN reťazca // Získanie aktuálnej FEN pozície
-    console.log("Current FEN:", currentFen);
-    const options_2 = {
-      method: 'GET',
-      url:'http://localhost:8080/openings',
-    };
-    try {
-      const response_2 = await axios.request(options_2);
-      const openings = response_2.data;
-      // Hľadáme zhodu s aktuálnou FEN pozíciou
-      const matchingOpening = openings.find(opening => opening.fen.split(' ')[0] === currentFenPosition);
-      if (matchingOpening) {
-        console.log("Match found:", matchingOpening);
-        setOpening(matchingOpening); // Uložiť celé otvorenie, alebo len časť podľa potreby
-    } else {
-        console.log("No matching opening found");
-        setOpening(null); // Alebo akúkoľvek inú predvolenú akciu, keď sa zhoda nenájde
-    }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  // Funkcia na zmenu orientácie šachovnice
+  const toggleBoardOrientation = () => {
+    setBoardOrientation(boardOrientation === 'white' ? 'black' : 'white');
+};
+
+
+  
   
   
 
@@ -63,40 +56,113 @@ function App() {
       console.error(error);
   }
 }
+async function findBestMove() {
+  if (!isActive) return;
+  const position = game.fen();
+
+  const options_2 = {
+    method: 'GET',
+    url: 'https://stockfish.online/api/s/v2.php',
+    params: {
+      fen: position,
+      depth: depth,
+    },
+  };
+
+  try {
+    const response = await axios.request(options_2);
+    if (response.data && response.data.success) {
+      // Assuming the best move is in the format "bestmove [move] ponder [move]"
+      // We split the string and take the second element (the move)
+      const bestMove = response.data.bestmove.split(' ')[1];
+      if (bestMove) {
+        // Apply the best move to the game. This assumes your game object
+        // has a method called 'move' that accepts moves in the format { from: 'b7', to: 'b6' }
+        game.move({
+          from: bestMove.substring(0, 2),
+          to: bestMove.substring(2, 4),
+        });
+
+
+        // Update your game's position. This assumes you have a method setGamePosition
+        // that takes the current game FEN string to update the app's state
+        setGamePosition(game.fen());
+        await getOpening();
+        console.log("Checking game over state:", game.game_over());
+        if (game.game_over()) {
+        const result = game.in_draw() ? 'Draw' : (game.in_checkmate() ? 'Checkmate' : 'Game Over');
+        console.log("Game result:", result);
+        setResultMessage(result);
+        setShowModal(true);
+      }
+        
+      }
+    } else {
+      console.error('No move found or error in response:', response.data);
+    }
+  } catch (error) {
+    console.error('Error finding best move:', error);
+  }
+}
   
 
-  async function findBestMove() {
-    if(!isActive) return;
-const encodedParams = new URLSearchParams();
-const posiotion_Fen = game.fen();
-encodedParams.set('fen', posiotion_Fen);
+//   async function findBestMove() {
+//     if(!isActive) return;
+// const encodedParams = new URLSearchParams();
+// const posiotion_Fen = game.fen();
+// encodedParams.set('fen', posiotion_Fen);
 
-const options = {
-  method: 'POST',
-  url: 'https://chess-stockfish-16-api.p.rapidapi.com/chess/api',
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded',
-    'X-RapidAPI-Key': 'a1bcd16d0fmsh42fe71c09c226b8p1cb35bjsn0ae77f4b35dd',
-    'X-RapidAPI-Host': 'chess-stockfish-16-api.p.rapidapi.com'
-  },
-  data: encodedParams,
-};
+// const options = {
+//   method: 'POST',
+//   url: 'https://chess-stockfish-16-api.p.rapidapi.com/chess/api',
+//   headers: {
+//     'content-type': 'application/x-www-form-urlencoded',
+//     'X-RapidAPI-Key': 'a1bcd16d0fmsh42fe71c09c226b8p1cb35bjsn0ae77f4b35dd',
+//     'X-RapidAPI-Host': 'chess-stockfish-16-api.p.rapidapi.com'
+//   },
+//   data: encodedParams,
+// };
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-   // Extract the best move from the response
-   const bestMove = response.data.bestmove;
-   // Apply the best move to the game
-   game.move({
-    from: bestMove.substring(0, 2),
-    to: bestMove.substring(2, 4),
-  });
-  setGamePosition(game.fen());
-} catch (error) {
-	console.error(error);
-}
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+//    // Extract the best move from the response
+//    const bestMove = response.data.bestmove;
+//    // Apply the best move to the game
+//    game.move({
+//     from: bestMove.substring(0, 2),
+//     to: bestMove.substring(2, 4),
+//   });
+//   setGamePosition(game.fen());
+//   await getOpening();
+// } catch (error) {
+// 	console.error(error);
+// }
 
+//   }
+  async function getOpening() {
+    const currentFen = game.fen();
+    const currentFenPosition = currentFen.split(' ')[0]; // Berieme len prvú časť FEN reťazca // Získanie aktuálnej FEN pozície
+    console.log("Current FEN:", currentFen);
+    const options_2 = {
+      method: 'GET',
+      url:'http://localhost:8080/openings',
+    };
+    try {
+      const response_2 = await axios.request(options_2);
+      const openings = response_2.data;
+      // Hľadáme zhodu s aktuálnou FEN pozíciou
+      const matchingOpening = openings.find(opening => opening.fen.split(' ')[0] === currentFenPosition);
+      if (matchingOpening) {
+        console.log("Match found:", matchingOpening);
+        setOpening(matchingOpening); // Uložiť celé otvorenie, alebo len časť podľa potreby
+    } else {
+        console.log("No matching opening found");
+        setOpening(null); // Alebo akúkoľvek inú predvolenú akciu, keď sa zhoda nenájde
+    }
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   function onDrop(sourceSquare, targetSquare, piece) {
@@ -111,55 +177,85 @@ try {
     if (move === null) return false;
 
     // exit if the game is over
-    if (game.game_over() || game.in_draw()) return false;
+    if (!game.game_over() && !game.in_draw()) {
+      findBestMove();
+      evaluatePosition();
+    }else{
+      const result = game.in_draw() ? 'Draw' : (game.in_checkmate() ? 'Checkmate' : 'Game Over');
+      setResultMessage(result);
+      setShowModal(true);
+    }
 
-    findBestMove();
-    evaluatePosition();
-    getOpening();
+    
+    //getOpening();
 
     return true;
   }
   return (
-    <div style={boardWrapper}>
-      
-
-      <Chessboard
-        id="PlayVsStockfish"
-        position={gamePosition}
-        onPieceDrop={onDrop}
-      />
-
-      <button
-        style={buttonStyle}
-        onClick={() => {
-          game.reset();
-          setGamePosition(game.fen());
-        }}
-      >
-        New game
-      </button>
-      <button
-        style={buttonStyle}
-        onClick={() => {
-          game.undo();
-          game.undo();
-          setGamePosition(game.fen());
-        }}
-      >
-        Undo
-      </button>
-      <button onClick={() => setIsActive(!isActive)} 
-      style={isActive ? stockfishMoveButton.activeButton : stockfishMoveButton.inactiveButton}>
-        {isActive ? 'Deactivate ' : 'Activate '} 
-        Best Move Finder
-      </button>
-      <div>
-        <p>Evaluation : {evaluation}</p>
-        <p>Opening: {opening ? opening.name : 'Unknown'}</p>
+    <div style={appContainer}>
+      <div style={sideContainer}>
+        <button style={buttonStyle} onClick={() => { game.reset(); setGamePosition(game.fen()); }}>
+          New game
+        </button>
+        <button style={buttonStyle} onClick={() => { game.undo(); game.undo(); setGamePosition(game.fen()); }}>
+          Undo
+        </button>
+        <button onClick={() => setIsActive(!isActive)} style={isActive ? stockfishMoveButton.activeButton : stockfishMoveButton.inactiveButton}>
+          {isActive ? 'Deactivate ' : 'Activate '} Best Move Finder
+        </button>
+        <button style={buttonStyle} onClick={toggleBoardOrientation}>
+          Rotate Board
+        </button>
       </div>
-    </div>
   
+      <div style={chessboardContainer}>
+        <Chessboard
+          id="PlayVsStockfish"
+          position={gamePosition}
+          onPieceDrop={onDrop}
+          boardOrientation={boardOrientation}
+          style={{ width: '90%', height: 'auto' }} // Ensuring the chessboard is responsive within its container
+        />
+      </div>
+  
+      <div style={sideContainer}>
+        <p>Evaluation: {evaluation}</p>
+        <p>Opening: {opening ? opening.name : 'Unknown'}</p>
+        <div>
+          <label htmlFor="depth">Depth:</label>
+          <input
+            id="depth"
+            type="number"
+            value={depth}
+            onChange={(e) => setDepth(Math.min(15, Math.max(1, parseInt(e.target.value, 10))))}
+            min="1"
+            max="15"
+          />
+        </div>
+      </div>
+      <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+            <Modal.Header closeButton>
+                <Modal.Title>Game Result</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{resultMessage}</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => setShowModal(false)}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={() => {
+                    setShowModal(false);
+                    game.reset();
+                    setGamePosition(game.fen());
+                    // Akékoľvek ďalšie akcie na reštart hry
+                }}>
+                    New Game
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </div>
   );
+  
+  
 }
 
 export default App;
--- src/style.js
+++ src/style.js
@@ -40,3 +40,33 @@ export const boardWrapper = {
     cursor: 'pointer',
     }
   };
+  export const appContainer = {
+    display: 'flex',
+    justifyContent: 'space-around', // Space items appropriately
+    alignItems: 'center', // Align items vertically in the middle
+    minHeight: '100vh', // Fill at least the entire height of the viewport
+    padding: '10px', // Add padding around the entire app container
+    flexWrap: 'wrap' // Allow items to wrap to next line if not enough space
+  };
+  
+  
+  export const chessboardContainer = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxWidth: '500px', // Maximum size of the chessboard
+    width: '100%', // Make the chessboard responsive
+    margin: '0 20px', // Add some margin to separate it from the buttons and other info
+  };
+  export const sideContainer = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center', // Center items horizontally in this column
+    justifyContent: 'start', // Align items to the start vertically
+    padding: '20px',
+    flex: '1 1 auto', // Allow these containers to grow and shrink as needed
+    maxWidth: '200px', // Set a maximum width to keep the UI tidy
+  };
+  
+  
+    
